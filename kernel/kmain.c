e0381858d2b1fe6f1e01e2e975c5a9cae2152bbb
Halil Yiğit <yhalil474@gmail.com>
2025-08-18 20:10:51 +0300

    .....

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,7 +1,7 @@
 #include <debug/debug.h>
 #include <keyboard/Keyboard.h>
 #include <mouse/mouse.h>
-
+#include <irq/IRQ.h>
 
 void kmain() {
 
@@ -15,6 +15,12 @@ void kmain() {
             LOG("Cursor moved: X=%d, Y=%d", cursor_X, cursor_Y);
             last_cursor_X = cursor_X;
             last_cursor_Y = cursor_Y;
+        }else {
+            for (size_t i = 0; i < 0xFFFFFFF; i++) {
+                // Busy wait to avoid flooding the log
+                asm volatile ("nop");
+            }
+            LOG("Cursor not moved!");
         }
     }
 
